c84b9a611af65650749b1c1c35577c6b79c47d65
Alejandro Tejada <tej17584@uvg.edu.gt>
2019-11-24 20:59:36 -0600

    Segundo commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hola nene</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './mainEstilos.scss';
+import App from "./components/App";
 
-const App = () => {
-  return <div>Hello React,Webpack 4 & Babel 7!</div>;
-};
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root")); 
